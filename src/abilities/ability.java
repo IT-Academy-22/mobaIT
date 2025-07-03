af000eccc1e78bb9ea4665a849f0c2d23c81dca3
Marcela Nunes Zarichta <marcela.zarichta@edu.pucrs.br>
2025-07-03 15:45:21 -0300

    Listas das habilidades de cada classe

--- src/abilities/ability.java
+++ src/abilities/ability.java
@@ -3,18 +3,20 @@ package abilities;
 import java.util.ArrayList;
 import java.util.List;
 
+import habilities.Ability;
+
 enum costType {
     MANA,
     HP;
 }
 
-public class ability {
+public class Ability {
     private String name;
     private String description;
     private int cost;
     private costType type;
 
-    public ability(String name, String description, int cost, costType type) {
+    public Ability(String name, String description, int cost, costType type) {
         this.name = name;
         this.description = description;
         this.cost = cost;
@@ -45,4 +47,38 @@ public class ability {
         habilities.add(new ability("Flame Arrow", "", 20, costType.MANA));
         return habilities;
     }
+
+    public List<Ability> assassinAbilities(){
+        List<Ability> abilities = new ArrayList<>();
+        abilities.add(new Ability("Stealth", "", 10, costType.MANA));
+        abilities.add(new Ability("Backstab", "", 10, costType.MANA));
+        abilities.add(new Ability("Bleed", "", 10, costType.MANA));
+        abilities.add(new Ability("Poison", "", 10, costType.MANA));
+    }
+
+    public List<Ability> tankAbilities(){
+        List<Ability> abilities = new ArrayList<>();
+        abilities.add(new Ability("Guardian´s Pace", "", 10, costType.MANA));
+        abilities.add(new Ability("Maximum Defense", "", 10, costType.MANA));
+        abilities.add(new Ability("Shield Bash", "", 10, costType.MANA));
+        abilities.add(new Ability("Desenfer´s Oath", "", 10, costType.MANA));
+    }
+
+    public List<Ability> supportAbilities(){
+        List<Ability> abilities = new ArrayList<>();
+        abilities.add(new Ability("Heal", "", 10, costType.MANA));
+        abilities.add(new Ability("Magic Shield", "", 10, costType.MANA));
+        abilities.add(new Ability("Encourage", "", 10, costType.MANA));
+        abilities.add(new Ability("Speed Boost", "", 10, costType.MANA));
+    }
+
+    public List<Ability> mageAbilities(){
+        List<Ability> abilities = new ArrayList<>();
+        abilities.add(new Ability("FIRE BALL!!!", "", 10, costType.MANA));
+        abilities.add(new Ability("Lightning Chains", "", 10, costType.MANA));
+        abilities.add(new Ability("Summon Roots", "", 10, costType.MANA));
+        abilities.add(new Ability("Polymorth", "", 10, costType.MANA));
+    }
+    
 }
+
