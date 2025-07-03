4a2d0e19dedff93abca0d03929ab0f57bf5d8e61
NicholasAstor <nicholas.barbosa@edu.pucrs.br>
2025-07-03 14:54:39 -0300

    Adding constructors and getters

--- src/habilidades/Habilidade.java
+++ src/habilidades/Habilidade.java
@@ -9,4 +9,27 @@ public class Habilidade {
     private String descricao;
     private TipoDoCusto tipoCusto;
     private int custo;
+
+    public Habilidade(String nome, String descricao, TipoDoCusto tipoCusto, int custo) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.tipoCusto = tipoCusto;
+        this.custo = custo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public TipoDoCusto getTipoCusto() {
+        return tipoCusto;
+    }
+
+    public int getCusto() {
+        return custo;
+    }
 }
