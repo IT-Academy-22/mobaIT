e7d0296e9133f19bb695fff245ac0a54c296b698
Joao Vithor Moraes Goncalves <JoaoVithor.Goncalves@dellteam.com>
2025-07-03 16:08:03 -0300

    Class Sorcerer created.

--- src/game_classes/Sorcerer.java
+++ src/game_classes/Sorcerer.java
@@ -1,5 +1,20 @@
 package game_classes;
 
-public class Sorcerer  extends CharacterClass {
-    
+import java.util.Arrays;
+
+public class Sorcerer extends CharacterClass {
+
+    public Sorcerer(String name) {
+        super();
+
+        setName(name);
+        setMaxHp(1200);
+        setBaseAttack(400); 
+        setBaseDefense(100); 
+        setResource(300); 
+        setSpeed(80); 
+
+        // setSkills(Arrays.asList("Bola de Fogo", "Teleporte", "Barreira Arcana"));
+        // setWeapons(Arrays.asList("Cajado Arcano", "Livro de Magias"));
+    }
 }
