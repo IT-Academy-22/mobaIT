4be0752a37591c7d51842437bd9f076f6595f97b
Joao Vithor Moraes Goncalves <JoaoVithor.Goncalves@dellteam.com>
2025-07-03 16:14:19 -0300

    Class Support created.

--- src/game_classes/Support.java
+++ src/game_classes/Support.java
@@ -1,5 +1,20 @@
 package game_classes;
 
+import java.util.Arrays;
+
 public class Support extends CharacterClass {
-    
+
+    public Support(String name) {
+        super(); 
+
+        setName(name);
+        setMaxHp(2000); 
+        setBaseAttack(100); 
+        setBaseDefense(300);
+        setResource(400); 
+        setSpeed(100);
+
+        // setSkills(Arrays.asList("Cura em Área", "Escudo de Luz", "Benção da Vida"));
+        // setWeapons(Arrays.asList("Cajado Sagrado", "Orbe da Harmonia"));
+    }
 }
