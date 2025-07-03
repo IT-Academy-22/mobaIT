f25b9398ceff6e5fdbd6557743335fc686b22904
Joao Vithor Moraes Goncalves <JoaoVithor.Goncalves@dellteam.com>
2025-07-03 16:10:29 -0300

    Class Hunter created.

--- src/game_classes/Hunter.java
+++ src/game_classes/Hunter.java
@@ -1,5 +1,20 @@
 package game_classes;
 
-public class Hunter  extends CharacterClass {
-    
+import java.util.Arrays;
+
+public class Hunter extends CharacterClass {
+
+    public Hunter(String name) {
+        super(); 
+
+        setName(name);
+        setMaxHp(1800); 
+        setBaseAttack(300); 
+        setBaseDefense(200); 
+        setResource(150); 
+        setSpeed(120); 
+
+        // setSkills(Arrays.asList("Tiro Preciso", "Armadilha de Urso", "Camuflagem"));
+        // setWeapons(Arrays.asList("Arco Longo", "Adagas Leves"));
+    }
 }
