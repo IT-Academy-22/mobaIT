15768a271b72835baf364e9433dc14664db3d1a0
Gustavo Hillesheim Teixeira <g.hillesheim@edu.pucrs.br>
2025-07-03 16:13:19 -0300

    Weapons class added

---
+++ src/weapon/Weapons.java
@@ -0,0 +1,11 @@
+package weapon;
+
+import java.util.List;
+
+public class Weapons {
+    List<Weapon> weapons;
+    
+    public Weapons(){
+        weapons.add(new Sword("Destruidora de galaxias", 10*3));
+    }
+}
