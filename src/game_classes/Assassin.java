76b54e1fbe2c90bbef2dbf88beb92d80c0e2cb89
Joao Vithor Moraes Goncalves <JoaoVithor.Goncalves@dellteam.com>
2025-07-03 16:13:12 -0300

    Class Assassin created.

--- src/game_classes/Assassin.java
+++ src/game_classes/Assassin.java
@@ -1,5 +1,20 @@
 package game_classes;
 
+import java.util.Arrays;
+
 public class Assassin extends CharacterClass {
-    
+
+    public Assassin(String name) {
+        super(); 
+
+        setName(name);
+        setMaxHp(1000);
+        setBaseAttack(450); 
+        setBaseDefense(100);
+        setResource(150); 
+        setSpeed(150); 
+
+        // setSkills(Arrays.asList("Ataque Fantasma", "Crítico Mortal", "Desaparecer"));
+        // setWeapons(Arrays.asList("Lâminas Gêmeas", "Shuriken"));
+    }
 }
