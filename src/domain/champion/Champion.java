3e02d7b4a9d305ea1e93b8f0b29ef77eb5dc2556
Fernando Gazzana <Fernando.Silva3@dellteam.com>
2025-07-03 15:56:59 -0300

    Add Champion Structure

---
+++ src/domain/champion/Champion.java
@@ -0,0 +1,35 @@
+package domain.champion;
+
+public class Champion {
+    private final String name;
+    private final Skill[] skill;
+    private int HP;
+    private int resource;
+    private ResourceType resourceType;
+    private int attack;
+    private int defense;
+    private int speed;
+    private final Race race;
+    private final Role role;
+    private Weapon weapon = null;
+
+    public Champion(String name, Skill[] skill, int HP, int resource, ResourceType resourceType, int attack, int defense, Race race, Role role) {
+        this.name = name;
+        this.skill = skill;
+        this.HP = HP + role.HP;
+        this.resource = resource + role.resource;
+        this.attack = attack + role.attack;
+        this.defense = defense + role.defense;
+        this.race = race;
+        this.role = role;
+        this.resourceType = role.resourceType.equals("HP") ? this.HP : role.resourceType;
+    }
+
+    private void equipWeapon(Weapon weapon){
+        this.weapon = weapon;
+    }
+
+
+
+
+}
