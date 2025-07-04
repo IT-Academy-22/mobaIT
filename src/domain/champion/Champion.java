fff2a295f607f41306db8ad568d1949f23e8771b
Matheus Braun Boff <matheusbraunboff@gmail.com>
2025-07-04 10:30:34 -0300

    Adição de getters e setters

--- src/domain/champion/Champion.java
+++ src/domain/champion/Champion.java
@@ -30,7 +30,76 @@ public class Champion {
         this.weapon = weapon;
     }
 
+    public String getName() {
+        return this.name;
+    }
 
 
+    public Skill[] getSkill() {
+        return this.skill;
+    }
+
+
+    public int getHP() {
+        return this.HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    public int getResource() {
+        return this.resource;
+    }
+
+    public void setResource(int resource) {
+        this.resource = resource;
+    }
+
+    public ResourceType getResourceType() {
+        return this.resourceType;
+    }
+
+    public void setResourceType(ResourceType resourceType) {
+        this.resourceType = resourceType;
+    }
+
+    public int getAttack() {
+        return this.attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return this.defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getSpeed() {
+        return this.speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public Race getRace() {
+        return this.race;
+    }
+
+
+    public Role getRole() {
+        return this.role;
+    }
+
+
+    public Weapon getWeapon() {
+        return this.weapon;
+    }
 
 }
